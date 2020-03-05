b41a47f24c66bd22c0246abf324466c904b6df77
Kenefiss <kenefiss@gmail.com>
2020-03-05 17:09:27 +0200

    added header section

--- gulpfile.js
+++ gulpfile.js
@@ -8,22 +8,27 @@ let gulp = require('gulp'),
   cssmin = require('gulp-cssmin');
 
 gulp.task('sass', function () {
-  return gulp.src('app/scss/style.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(rename({ suffix: '.min' }))
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 8 versions']
     }))
     .pipe(gulp.dest('app/css'))
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 gulp.task('style', function () {
   return gulp.src([
-    'node_modules/normalize.css/normalize.css',
-    'node_modules/slick-carousel/slick/slick.css',
-    'node_modules/magnific-popup/dist/magnific-popup.css'
-  ])
+      'node_modules/normalize.css/normalize.css',
+      'node_modules/slick-carousel/slick/slick.css'
+    ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
     .pipe(gulp.dest('app/css'))
@@ -31,9 +36,8 @@ gulp.task('style', function () {
 
 gulp.task('script', function () {
   return gulp.src([
-    'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
-  ])
+      'node_modules/slick-carousel/slick/slick.js'
+    ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('app/js'))
@@ -41,12 +45,16 @@ gulp.task('script', function () {
 
 gulp.task('html', function () {
   return gulp.src('app/*.html')
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 gulp.task('js', function () {
   return gulp.src('app/js/*.js')
-    .pipe(browserSync.reload({ stream: true }))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
 });
 
 gulp.task('browser-sync', function () {
@@ -58,7 +66,7 @@ gulp.task('browser-sync', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('app/scss/style.scss', gulp.parallel('sass'))
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'))
   gulp.watch('app/*.html', gulp.parallel('html'))
   gulp.watch('app/js/*.js', gulp.parallel('js'))
 });
