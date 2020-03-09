2d51f6130349901c4b375a0d0a68af69a8b5c736
Kenefiss <kenefiss@gmail.com>
2020-03-09 22:36:48 +0200

    finished slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,12 @@
 $(function () {
 
   $('.slider__inner').slick({
-    slidesToShow: 6,
+    slidesToShow: 5,
     slidesToScroll: 2,
+    nextArrow: '<button class="slick-arrow slick-next"><img src="/images/next.png"></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/prev.png"></button>',
+    dots: true,
     autoplay: true,
-    autoplaySpeed: 3000,
-    centerMode: true,
   });
 
 
