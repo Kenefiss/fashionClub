f4333d31e503a7a60989a87bd960df6ade2a88f6
Kenefiss <kenefiss@gmail.com>
2020-03-16 17:21:02 +0200

    finished all website

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,42 @@ $(function () {
     dots: true,
     autoplay: true,
     autoplaySpeed: 5000,
-    infinite: true
+    focusOnSelect: true,
+    infinite: true,
+    responsive: [{
+        breakpoint: 1366,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          autoplay: false
+        }
+      },
+      {
+        breakpoint: 1085,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          autoplay: false
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          autoplay: false
+        }
+      },
+      {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false,
+          autoplay: false
+        }
+      }
+    ]
   });
 
   $(window).scroll(function () {
@@ -20,7 +55,9 @@ $(function () {
     }
   });
 
-  new WOW().init();
+  new WOW({
+    mobile: false
+  }).init();
 
   $('.header__menu-btn').on('click', function () {
     $('.header__menu ul').slideToggle(1112);
@@ -30,10 +67,5 @@ $(function () {
       $('.header__menu ul').removeAttr('style');
     }
   });
-
-
-
-
-
 
 });
