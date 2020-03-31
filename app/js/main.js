c45e6aa852661aff6745de0f09f2376edda1bfc4
Kenefiss <kenefiss@gmail.com>
2020-03-31 13:17:47 +0300

    bug fix

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,8 @@ $(function () {
   $('.slider__inner').slick({
     slidesToShow: 5,
     slidesToScroll: 2,
-    nextArrow: '<button class="slick-arrow slick-next"><img src="/images/next.png"></button>',
-    prevArrow: '<button class="slick-arrow slick-prev"><img src="/images/prev.png"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.png"></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.png"></button>',
     dots: true,
     autoplay: true,
     autoplaySpeed: 5000,
